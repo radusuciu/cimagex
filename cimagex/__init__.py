7de2778dd2510d7a5c70b90762790ed519f093d4
Radu Suciu <radusuciu@gmail.com>
2017-06-12 13:45:48 -0700

    Module re-org

--- cimagex/__init__.py
+++ cimagex/__init__.py
@@ -1,3 +1,5 @@
-from .dataset import Dataset, make_mudpit_dataset
-from .peptide_dataset import PeptideDataset, make_isotop_dataset
+from .dataset import Dataset
+from .dataset import make_dataset as make_mudpit_dataset
+from .peptide_dataset import PeptideDataset
+from .peptide_dataset import make_dataset as make_isotop_dataset
 from .parse_combined import ParseCombined
