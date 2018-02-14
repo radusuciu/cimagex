d41706db00e444bc1e8e69adfb0b9e78804668f8
Radu Suciu <radusuciu@gmail.com>
2018-02-13 18:02:31 -0800

    Update heatmap

--- cimagex/heatmap.py
+++ cimagex/heatmap.py
@@ -1,23 +1,37 @@
 from matplotlib.pyplot import cm
 import matplotlib.pyplot as plt
+import matplotlib
 import pathlib
 import csv
 
 
 def heatmap(ratios, x_labels, y_labels, output_path):
-    fig, ax = plt.subplots()
-    fig.set_size_inches(6, 15, forward=True)
-    im = ax.imshow(ratios, interpolation='nearest',  cmap=cm.Blues)
+    # matplotlib.rcParams.update({
+    #     'font.family': 'sans-serif',
+    #     'font.sans-serif': 'Univers LT Std 55',
+    #     'svg.fonttype': 'svgfont'
+    # })
+
+    fig = plt.gcf()
+    ax = plt.subplot(131)
+
+    fig.set_size_inches(9, 9, forward=True)
+    im = ax.imshow(ratios, interpolation='nearest', cmap=cm.Blues, vmin=0, vmax=20, aspect='auto')
 
     ax.set_yticks(range(len(y_labels)))
-    ax.set_yticklabels(y_labels, family='sans-serif', size=10)
+    ax.set_yticklabels(y_labels, size=10)
 
     ax.get_xaxis().tick_top()
     plt.xticks(rotation=90)
     ax.set_xticks(range(len(x_labels)))
     ax.set_xticklabels(x_labels)
 
+    cbar = fig.colorbar(im, ax=ax, orientation='horizontal', pad=0.02, ticks=[0, 5, 10, 15, 20])
+    cbar.ax.set_ylabel('R', rotation='horizontal', va='center')
+    cbar.ax.get_yaxis().set_label_coords(-0.1, 0.35)
+
     plt.tight_layout()
+
     plt.savefig(str(pathlib.Path(output_path).with_suffix('.svg')), format='svg')
 
     with pathlib.Path(output_path).with_suffix('.csv').open('w', encoding='UTF-8') as f:
